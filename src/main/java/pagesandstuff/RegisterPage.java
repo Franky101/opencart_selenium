0a60bd93d46c2a63635743ad550a10add78123cb
Franky101 <becerra.frn@gmail.com>
2023-09-05 20:47:29 -0300

    feat: testng.xml added with Suites and RegisterTest.java created as well.

--- src/main/java/pagesandstuff/RegisterPage.java
+++ src/main/java/pagesandstuff/RegisterPage.java
@@ -1,6 +1,9 @@
 package pagesandstuff;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -9,15 +12,40 @@ public class RegisterPage {
 
     // Locators
 
+    // H1 header "Account"
+    private By accountHeader = By.xpath("//h1[text()='Account']");
+
     // First name
+    private By firstNameField = By.id("input-firstname");
     // Last Name
+    private By lastNameField = By.id("input-lastname");
     // Email
+    private By emailField = By.id("input-email");
     // Telephone
+    private By telephoneField = By.id("input-telephone");
     // Password
+    private By pswdField = By.id("input-password");
     // Password Confirm
-    // Nesletter
+    private By pswdConfirmField = By.id("input-confirm");
+    // Newsletter
+    private By newsletterCheck = By.xpath("//*[@id=\"content\"]/form/fieldset[3]/div/div/label[2]/input");
     // Agreement check
+    private By agreeCheck = By.xpath("//input[@type='checkbox']");
     // Submit Button
+    private By submitBtn = By.xpath("//*[@type='submit']");
+
+
+    // Confirm Account Creation Text
+    private By congratText = By.xpath("//*[contains(text(),'Congratulations! Your new account has been successfully created!')]");
+    // Continue Button
+    private By continueBtn = By.xpath("//button[text()='Continue']");
+
+
+    // Account Control Panel Elements: #Note: This should go to a different test and page
+    private By myAccountHeader = By.xpath("//h2[text()='My Account']");
+    private By myOrdersHeader = By.xpath("//h2[text()='My Orders']");
+    private By myAffiliate = By.xpath("//h2[text()='My Affiliate Account']");
+    private By myNewsletterHeader = By.xpath("//h2[text()='Newsletter']");
 
 
 
@@ -28,8 +56,63 @@ public class RegisterPage {
     // Constructor
     public RegisterPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
+
+    // Methods
+
+    public boolean headerElementVisible() {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(accountHeader));
+        return element.isDisplayed();
+    }
+    public void firstNameInput(String firstName) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(firstNameField));
+        element.sendKeys(firstName);
+    }
+    public void lastNameInput(String lastName) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(lastNameField));
+        element.sendKeys(lastName);
+    }
+    public void emailInput(String email) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(emailField));
+        element.sendKeys(email);
+    }
+    public void telephoneInput(String telephoneNumber) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(telephoneField));
+        element.sendKeys(telephoneNumber);
+    }
+    public void paswdInput(String password) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(pswdField));
+        element.sendKeys(password);
+    }
+    public void paswdConfirmInput(String password) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(pswdConfirmField));
+        element.sendKeys(password);
+    }
+    public void newsLetterClick() {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(newsletterCheck));
+        element.click();
+    }
+
+    public void agreeCheckClick() {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(agreeCheck));
+        element.click();
+    }
+    public void SubmitBtnClick() {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(submitBtn));
+        element.click();
+    }
+
+    public boolean confirmAccount() {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(congratText));
+        return element.isDisplayed();
+    }
+    public void continueBtnClick() {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(continueBtn));
+        element.click();
+    }
+
+
 
 }
 
--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import baseFiles.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pagesandstuff.MainPage;
 import pagesandstuff.RegisterPage;
@@ -8,16 +9,44 @@ import pagesandstuff.RegisterPage;
 public class RegisterTest extends BaseTest {
 
     @Test
-    public void registerCorrect(){
+    public void openRegisterPage(){
         MainPage mainPage = new MainPage(getDriver());
         RegisterPage registerPage = new RegisterPage(getDriver());
+
         mainPage.openRegister();
+        registerPage.headerElementVisible();
+    }
 
-        registerPage.
+    @Test(dependsOnMethods = "openRegisterPage")
+    public void fillRegisterPage() {
 
+        String baseEmail = "UserTest";
+        String email = baseEmail + System.currentTimeMillis() + "@gmail.com";
+
+        RegisterPage registerPage = new RegisterPage(getDriver());
+        registerPage.firstNameInput("Franco");
+        registerPage.lastNameInput("Becerra");
+        registerPage.emailInput(email);
+        registerPage.telephoneInput("+54 9 351-48655153");
+        registerPage.paswdInput("123456789");
+        registerPage.paswdConfirmInput("123456789");
+        registerPage.newsLetterClick();
+        registerPage.agreeCheckClick();
+        registerPage.SubmitBtnClick();
+    }
+    @Test(dependsOnMethods = "fillRegisterPage")
+    public void confirmAccountCreation() {
+        RegisterPage registerPage = new RegisterPage(getDriver());
+        Assert.assertTrue(registerPage.confirmAccount());
+//        registerPage.continueBtnClick();
     }
 
 
-//    RegisterPage registerPage = new RegisterPage(getDriver());
-//        homePage.accederAlLogin();
+    // Account Control Panel test from here forward.
+    @Test(dependsOnMethods = "confirmAccountCreation")
+    public void accountControlPanel(){
+        RegisterPage registerPage = new RegisterPage(getDriver());
+
+    }
+
 }
